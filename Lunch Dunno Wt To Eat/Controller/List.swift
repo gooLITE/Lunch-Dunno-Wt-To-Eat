a2238f6bf0d4971ffe704d060953020f379fbd34
gooLITE <goolitesum@gmail.com>
2021-10-05 01:07:59 +0800

    initial

---
+++ Lunch Dunno Wt To Eat/Controller/List.swift
@@ -0,0 +1,8 @@
+//
+//  List.swift
+//  Lunch Dunno Wt To Eat
+//
+//  Created by Yu Sum Yung on 4/10/2021.
+//
+
+import Foundation
---
+++ Lunch Dunno Wt To Eat/Controller/MainTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainTableViewCell.swift
+//  Lunch Dunno Wt To Eat
+//
+//  Created by Yu Sum Yung on 5/10/2021.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
